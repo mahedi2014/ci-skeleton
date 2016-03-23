56e9c3fcc190f1deaa8ad9eb8568419cee0a6b24
mahedi2014 <mahedi2014@gmail.com>
2016-03-23 12:21:27 +0600

    signin complete

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -12,6 +12,8 @@ class User extends CI_Controller {
         $this->load->helper('language');
         $this->load->library('form_validation');
 
+        $this->load->library('session');
+
         $this->load->model('user_model');
     }
 
@@ -22,7 +24,6 @@ class User extends CI_Controller {
 
     public function signup()
     {
-
         // create the data object
         $data = new stdClass();
 
@@ -48,13 +49,83 @@ class User extends CI_Controller {
                 $this->load->render_default('user/signup_success', $data);
 
             } else {
-
                 // user creation failed, this should never happen
                 $data->error = 'There was a problem creating your new account. Please try again.';
 
                 // send error to the view
                 $this->load->render_default('user/signup', $data);
             }
+        }
+    }
+
+
+    public function signin()
+    {
+        // create the data object
+        $data = new stdClass();
+
+        // load form helper and validation library
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        // set validation rules
+        $this->form_validation->set_rules('username', 'Username', 'required|alpha_numeric');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+        if ($this->form_validation->run() == false) {  // validation not ok, send validation errors to the view
+            $this->load->render_default('user/signin');
+
+        } else { // set variables from the form
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            if ($this->user_model->resolve_user_login($username, $password)) {
+
+                $user_id = $this->user_model->get_user_id_from_username($username);
+                $user    = $this->user_model->get_user($user_id);
+
+                // set session user data
+                $user_data = array(
+                    'user_id'   => (int)$user->id,
+                    'username'  => (string)$user->username,
+                    'email'     => (string)$user->email,
+                    'is_admin'  => (string)$user->is_admin,
+                    'is_superadmin'  => (string)$user->is_superadmin
+                );
+
+                $this->session->set_userdata($user_data);
+
+                //after session redirect
+                redirect(base_url().'dashboard/user/index','true');
+
+            } else {  // send error to the view
+                $data->error = 'Wrong username or password.';
+                $this->load->render_default('user/signin', $data);
+            }
+        }
+    }
+
+    /**
+     * signout function.
+     *
+     * @access public
+     * @return void
+     */
+    public function signout() {
+
+        // create the data object
+        $data = new stdClass();
+
+        $session_array = $this->session->all_userdata();
+
+        if ($this->session->unset_userdata($session_array)) { // remove session data
+            // user logout ok
+            $this->load->render_default('user/signin', $data);
+
+        } else {
+            // there user was not logged in, we cannot logged him out,
+            redirect('/');
+
         }
     }
 }
---
+++ application/controllers/dashboard/User.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+    public function __construct() {
+
+        parent::__construct();
+		$this->load->database();
+        $this->load->helper('html');
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->helper('language');
+        $this->load->library('form_validation');
+
+        $this->load->model('user_model');
+    }
+
+    public function index()
+    {
+        echo 'signin complete';
+    }
+
+
+}
