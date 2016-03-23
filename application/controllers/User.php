1f930277e21c847a6055b27097ab001830aecc3c
mahedi2014 <mahedi2014@gmail.com>
2016-03-23 10:21:49 +0600

    updated

---
+++ application/controllers/User.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+    public function __construct() {
+
+        parent::__construct();
+//		$this->load->database();
+        $this->load->helper('html');
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->helper('language');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $this->load->render_default('user/signup');
+    }
+}
--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -34,19 +34,4 @@ class MY_Loader extends CI_Loader
         endif;
     }
 
-    public function render_custom($template_name, $header = 'header', $footer = 'footer', $vars = array(), $return = FALSE)
-    {
-        if($return):
-            $content  = $this->view($header, $vars, $return);
-            $content .= $this->view($template_name, $vars, $return);
-            $content .= $this->view($footer, $vars, $return);
-
-            return $content;
-        else:
-            $this->view($header, $vars);
-            $this->view($template_name, $vars);
-            $this->view($footer, $vars);
-        endif;
-    }
-
 }
