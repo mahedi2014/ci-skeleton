bd3e1d02873b716d06bf3b1c7a59fb80a096bbb3
mahedi2014 <mahedi2014@gmail.com>
2016-03-23 13:52:41 +0600

    dashboard layout setting

--- application/controllers/user/Dashboard.php
+++ application/controllers/user/Dashboard.php
@@ -5,7 +5,7 @@ class Dashboard extends CI_Controller {
     public function __construct() {
 
         parent::__construct();
-		$this->load->database();
+        $this->load->database();
         $this->load->helper('html');
         $this->load->helper('form');
         $this->load->helper('url');
@@ -13,15 +13,18 @@ class Dashboard extends CI_Controller {
         $this->load->library('form_validation');
         $this->load->library('session');
 
-        $this->load->model('user_model');
+        $this->load->model('auth_model');
     }
 
     public function index()
     {
 
         if($this->session->userdata('username')) {
-           echo $this->session->userdata('username');
-            echo anchor(base_url().'auth/signout', 'Signout');
+            $data = new stdClass();
+            echo $this->session->userdata('username');
+//            echo anchor(base_url().'auth/signout', 'Signout');
+
+            $this->load->render_default('user/dashboard', $data);
 
         }else {
             redirect(base_url().'auth/signout','true');
--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -34,4 +34,21 @@ class MY_Loader extends CI_Loader
         endif;
     }
 
+    public function render_content($template_name, $vars = array(), $return = FALSE)
+    {
+        $header = 'header';
+        $footer = 'footer';
+        if($return):
+            $content  = $this->view($header, $vars, $return);
+            $content .= $this->view($template_name, $vars, $return);
+            $content .= $this->view($footer, $vars, $return);
+
+            return $content;
+        else:
+            $this->view($header, $vars);
+            $this->view($template_name, $vars);
+            $this->view($footer, $vars);
+        endif;
+    }
+
 }
