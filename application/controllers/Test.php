ea074b8487bf451ebbbc8b9ecab5707f8bcdec2a
mahedi2014 <mahedi2014@gmail.com>
2016-03-28 16:52:20 +0600

    test controller

---
+++ application/controllers/Test.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+    public function __construct() {
+
+        parent::__construct();
+		$this->load->database();
+        $this->load->helper('html');
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->helper('language');
+        $this->load->library('form_validation');
+        $this->load->library('session');
+        $this->load->model('auth_model');
+    }
+
+    public function index()
+    {
+
+    }
+
+
+}
