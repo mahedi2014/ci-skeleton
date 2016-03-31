d12a6500589a0b33183d77e8bca311d3a1f8d65f
mahedi2014 <mahedi2014@gmail.com>
2016-03-31 11:35:27 +0600

    email send with gmail complete

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -5,7 +5,6 @@ class Test extends CI_Controller
 {
     public function __construct()
     {
-
         parent::__construct();
         $this->load->database();
         $this->load->helper('html');
@@ -22,44 +21,13 @@ class Test extends CI_Controller
     public function index()
     {
 	    $mail = new Mail();
-	    $this->email_sending();
+	    $this->email_smtp();
     }
-
-	public function email_me()
-	{
-		$config = array(
-			'protocol' => 'smtp',
-			'smtp_host' => 'ssl://smtp.zoho.com',
-			'smtp_port' => 465,
-			'smtp_user' => 'noreply@mrcmanager.com', // change it to yours
-			'smtp_pass' => 'Energytec2013', // change it to yours
-			'mailtype' => 'html',
-			'charset' => 'iso-8859-1',
-			'user_name' => 'Manager',
-			'welcome_to' => 'Welcome!',
-			'wordwrap' => TRUE
-		);
-
-		$this->load->library('email', $config);
-		$this->email->set_newline("\r\n");
-		$this->email->from('noreply@mrcmanager.com', 'test'); // change it to yours
-		$this->email->to('mahedi2014@gmail.com');// change it to yours
-		$this->email->subject('Email Verification!');
-		$this->email->message('Test');
-		if($this->email->send())
-		{
-			echo "success";
-		}
-		else
-		{
-			echo "failed";
-		}
-	}
 
 	public function email_smtp()
 	{
 		$to = 'mahedi2014@gmail.com';
-		$from = 'noreply@mrcmanager.com';
+		$from = 'paytechbd@gmail.com';
 		$from_name = 'test';
 		$subject = 'test';
 		$body = 'test';
@@ -72,10 +40,10 @@ class Test extends CI_Controller
 		$mail->IsSMTP();
 		$mail->SMTPAuth = true;
 		$mail->SMTPSecure = 'ssl';
-		$mail->Host = 'ssl://smtp.zoho.com';
+		$mail->Host = 'ssl://smtp.gmail.com';
 		$mail->Port = 465;
-		$mail->Username = 'noreply@mrcmanager.com';
-		$mail->Password = 'Energytec2013';
+		$mail->Username = 'paytechbd@gmail.com';
+		$mail->Password = 'mahediazad';
 
 		$mail->SetFrom($from, $from_name);
 		$mail->Subject = $subject;
